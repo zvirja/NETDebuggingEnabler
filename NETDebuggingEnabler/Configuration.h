b12bac5d42b4ba4cb736500a93fb9325c2c688a3
Alex Povar <user.home.0000@gmail.com>
2013-10-11 00:57:12 +0300

    Change default module path filter value

--- NETDebuggingEnabler/Configuration.h
+++ NETDebuggingEnabler/Configuration.h
@@ -19,7 +19,7 @@ namespace Managers
 		void SetDisplayFullModulePaths(bool value) { SetBoolValue(KeyDisplayFullModulePaths, value); }
 		bool GetEnableModulePathFilter() { return GetBoolValue(KeyEnableModulePathFilter, false); }
 		void SetEnableModulePathFilter(bool value) { SetBoolValue(KeyEnableModulePathFilter, value); }
-		wxString GetModulePathFilter() { return GetStringValue(KeyModulePathFilter, L"*ASP.NET*"); }
+		wxString GetModulePathFilter() { return GetStringValue(KeyModulePathFilter, L"^*.DLL"); }
 		void SetModulePathFilter(const wxString& value) { SetStringValue(KeyModulePathFilter, value); }
 
 	private:
